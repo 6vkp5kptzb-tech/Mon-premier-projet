f8e1f47e1b6e0dd9022a0bb8e46928c7e2d4fb1c
6vkp5kptzb-tech <6vkp5kptzb@privaterelay.appleid.com>
2026-02-13 19:08:40 +0100

    Essai pour cursor

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ print ("Je bidouille sur Antigravity!!!")
 print (f"Bonjour, nous sommes le : {datetime.now().strftime('%d/%m/%Y')}")
 print ("Merci de m'aider à comprendre le fonctionnement de github")
 print ("Essai Cursor")
-
+print ("test")
 
 
