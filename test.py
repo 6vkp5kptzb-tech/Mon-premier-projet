f28a2612710cab9fe1f16dec406c689d2fc36ffb
6vkp5kptzb-tech <6vkp5kptzb@privaterelay.appleid.com>
2026-02-13 19:04:42 +0100

    essai sur cursor-Claude

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ print ("Nouvelle modif")
 print ("Je bidouille sur Antigravity!!!")
 print (f"Bonjour, nous sommes le : {datetime.now().strftime('%d/%m/%Y')}")
 print ("Merci de m'aider à comprendre le fonctionnement de github")
-
+print ("Essai Cursor")
 
 
 
